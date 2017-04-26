9ed3f8b7741005a13b070e8677833f03afa88691
Chip Webber <s231426@corp.aepsc.com>
2017-04-26 09:41:20 -0500

    new version after upstream merge

--- setup.py
+++ setup.py
@@ -7,5 +7,6 @@ from setuptools import setup
 setup(name='pssepath',
         description='A module to detect and setup paths required for Python for PSSE operation.',
         author='Daniel Hillier',
-        py_modules=['pssepath']
+        py_modules=['pssepath'],
+        version='0.8',
         )
